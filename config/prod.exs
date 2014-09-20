295e56d1e15a2e20bc45a159a2a0a12833e44aec
Aziz Shamim <azizshamim@gmail.com>
2014-09-20 14:53:19 -0500

    getting this working on heroku

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :phoenix, Fawkes.Router,
   port: System.get_env("PORT"),
   ssl: false,
-  host: "example.com",
+  host: "heroku-fup.herokuapp.com",
   cookies: true,
   session_key: "_fawkes_key",
   session_secret: "J0#%EFD!W1W=PS$5F_&(Z7_2I6D6R$DQ+H5!*@D93L9EIMH!GTOX+D)*FF2^@T#5%8%T+%&!RS9CZ"
